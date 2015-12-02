7a48838ae033bb7289147b6dd98d5ca12ecc04db
continnd <continnd@miamioh.edu>
2015-12-01 22:45:06 -0500

    Put Parser.cpp code into Architecture and Results classes, and added a run_benchmark method for running the specified benchmark in the Results file.

--- src/Architecture.cpp
+++ src/Architecture.cpp
@@ -43,4 +43,98 @@ Architecture& Architecture::operator=(Architecture&& other) {
 }
 /* }}} */
 
+void Architecture::make_arch_file(std::string& filename) {
+    std::string line, temp = "2.690e-10";
+    for (size_t i = 0; i < K - 1; i++) {
+        temp += "\n2.690e-10";
+    }
+    std::unordered_map<std::string, std::string> temp_args = {
+        {TEMP_K, "num_pins=\"" + std::to_string(K) + "\""},
+        {"TEMP_DELAY", temp}};
+    std::ifstream is("../arch_template.xml");
+    std::ofstream os(filename);
+    std::stringstream ss;
+    while (std::getline(is, line)) {
+        ss << line;
+        while (ss >> temp) {
+            auto it = temp_args.find(temp);
+            if (it != temp_args.end()) {
+                os << it->second;
+            } else {
+                os << temp;
+            }
+            os << " ";
+        }
+        os << std::endl;
+        ss.str(std::string());
+        ss.clear();
+    }
+    os.close();
+}
+
+Architecture::Results::Results()
+    : crit_path{UNSET}
+    , area{UNSET}
+    , filename{""}
+{ }
+
+Architecture::Results::~Results()
+{ }
+
+void Architecture::run_benchmark(unsigned K, unsigned I, unsigned W) {
+    std::string command = std::string(VPR_PATH) + " " + arch_file + benchmark
+        + "-route_chan_width " + W;
+    FILE* res;
+    double temp_area, temp_crit;
+    for (unsigned i = 0; i < BENCH_ITER; i++) {
+        res = popen(command.c_str());
+        if (crit_path == UNSET) {
+            std::tie(area, crit_path) = parse_results(res);
+        } else {
+            std::tie(temp_area, temp_crit) = parse_results(res);
+            area = std::min(area, temp_area);
+            crit_path = std::max(crit_path, temp_crit);
+        }
+    }
+}
+
+Architecture::Results::std::pair<double, double> parse_results(FILE* res) {
+    double metrics[NUM_METRICS];
+    std::regex reg[NUM_METRICS] = {std::regex(LOGIC_AREA),
+        std::regex(ROUTE_AREA), std::regex(CRIT_PATH)};
+    std::stringstream ss;
+    std::string temp;
+    char line[256];
+    // Search stream until all metrics are found
+    for (size_t i = 0; i < NUM_METRICS;) {
+        // If you get to end of stream, output failure
+        if (!fgets(line, 256, res)) {
+            return std::pair<double, double>(-1, -1);
+        }
+        // If the stat we are looking for is found, parse the line
+        if (std::regex_search(line, reg[i])) {
+            ss << line;
+            while (ss >> temp) {
+                // If the number is represented in scientific notation,
+                // parse it to output a double
+                if (std::regex_match(temp, std::regex(SCIENTIFIC_NOTATION))) {
+                    metrics[i] = atof(temp.substr(0, temp.find('e') + 1).c_str());
+                    int exp = atoi(temp.substr(temp.find('e') + 1).c_str());
+                    while (exp != 0) {
+                        metrics[i] *= 10;
+                        exp--;
+                    }
+                    break;
+                } else if (temp[0] >= '0' && temp[0] <= '9') {
+                    metrics[i] = atof(temp.c_str());
+                    break;
+                }
+            }
+            ss.str(std::string());
+            i++;
+        }
+    }
+    return std::pair<double, double>(metrics[0] + metrics[1], metrics[2]);
+}
+
 // TODO: Implement me!
--- src/Architecture.h
+++ src/Architecture.h
@@ -2,6 +2,25 @@
 #define ARCHITECTURE_H_
 
 #include <algorithm>
+#include <string>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <utility>
+#include <vector>
+#include <unordered_map>
+
+
+#define VPR_PATH
+#define CRIT_PATH "Final critical path:"
+#define LOGIC_AREA "Total used logic block area:"
+#define ROUTE_AREA "Total routing area:"
+#define SCIENTIFIC_NOTATION "[-]?[0-9]+\\.[0-9]+([e][-+][0-9]+)"
+#define NUM_METRICS 3
+#define TEMP_K "num_pins=TEMP_K"
+#define BENCH_ITER 10
 
 class Architecture {
 public:
@@ -41,8 +60,28 @@ public:
     // TODO: switch block type?
 
 private:
-};
+    void make_arch_file(std::string& filename);
+    class Results {
+        public:
+            Results();
+            ~Results();
+            void run_benchmark();
 
-// const unsigned Architecture::UNSET = 0;
+            /* Method that parses the output of vpr in the form of a string.
+             * res: stream of vpr results.
+             * output: a pair, with first representing area in minimum transistor units
+             *         and second representing critical path in ns. Return -1, -1 if all
+             *         metrics are not found.
+             */
+            std::pair<double, double> parse_results(FILE* res);
+
+        private:
+            double crit_path;
+            double area;
+            std::string benchmark;
+            std::string arch_file;
+    };
+
+    // const unsigned Architecture::UNSET = 0;
 
 #endif /* end of include guard */
